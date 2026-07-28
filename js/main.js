c83f87c0d0e4804ace208d26d5ec3419e5eb17b2
Athul Shaji <athulshaji.research@gmail.com>
2026-07-28 14:14:14 +0530

    Add main JavaScript file for academic website

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*
+===========================================================
+Athul Shaji Academic Website
+Main JavaScript
+Version 1.0
+===========================================================
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Athul Shaji Academic Website Loaded");
+
+});
